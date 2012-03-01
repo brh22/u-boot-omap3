4c120852d09af1d912c42e8372289c982f1fc24e
Delio Brignoli <dbrignoli@audioscience.com>
2012-03-01 19:33:03 +0100

    Merge initial support for ASI1230 from the v2010.06_TI814XPSP_04.01.00.06.patch1-dxb branch.
    Squashed commit of the following:
    
    commit f641f19158dc0e163dc110857af4d892486d7fc0
    Author: Delio Brignoli <dbrignoli@audioscience.com>
    Date:   Thu Mar 1 19:26:02 2012 +0100
    
        Added comment about DSP clock frequency (originally committed by SGT)
    
    commit bff2698a216eb6300508971cbe398c61802b7877
    Author: Delio Brignoli <dbrignoli@audioscience.com>
    Date:   Mon Feb 27 22:09:12 2012 +0100
    
        Split SD and UART versions for 1st and 2nd stage bootloader in Makefile
        Added ASI1230 version of PLL parameters header
        Fixed mDDR PLL settings
        Use GP[0] bits from 1 to 5 for 4 status LEDs
        Fix GP[0] clock init
        Fixed status LED init during board early setup
        Add a dummy memory access and a delay after mDDR setup
        Added back ETH support code
        Removed NAND related code
        Patch OMAP3 SPI driver to support reversing MISO and MOSI pins when CONFIG_OMAP3_SPI_REVERSE_DATAPINS is defined
        Configure 1st and 2nd stage to optionally support environment in SPI flash
    
    commit 0f8b1d3f143f678051b9653c332f14b878bb2e94
    Author: Delio Brignoli <dbrignoli@audioscience.com>
    Date:   Fri Feb 17 06:40:14 2012 +0100
    
        Fixed LED control logic (LEDs are ON when driven low). Use GPIO SET and CLEAR registers.
    
    commit f17087b04eb9c14488717b37d092ed324203e6b5
    Author: Delio Brignoli <dbrignoli@audioscience.com>
    Date:   Wed Dec 14 15:40:47 2011 +0100
    
        Removed unused memory access macros.
        Corrected global DRAM banks definitions.
        Initialize GPIO0.
        Added board specific LED API support.
        Turn on LED0 before DRAM init and turn on LED1 (and LED0 off) when done with DRAM init.
    
    commit 43b1b1589cfdad6ccf1d835818ed96c13246a1d2
    Author: Delio Brignoli <dbrignoli@audioscience.com>
    Date:   Tue Dec 13 18:51:58 2011 +0100
    
        Removed unused DDR2/DDR3 init code.
        Removed unused pinmux setup for NOR, CPSW, GPMC.
        Replaced EVM pinmux header with ASI1230 specific version.
    
    commit 0651714df6071760effb041fa821c4998e44ce60
    Author: Delio Brignoli <dbrignoli@audioscience.com>
    Date:   Tue Dec 13 16:57:58 2011 +0100
    
        Add ASI1230 board support

---
+++ board/asi1230/ddr_defs.h
@@ -0,0 +1,85 @@
+
+#include <asm/arch/ddr_defs_ti814x.h>
+
+/* Values taken and adapterd from "IS43LR32160B: 4M x 32Bits x 4Banks Mobile
+   DDR SDRAM" datasheet. mDDR is clocked at 166MHz
+*/
+
+#define CAS_LATENCY (3)
+#define mDDR_CLK_FREQ (166)
+#define mDDR_CLK_PERIOD (6)
+
+#define DYN_PWRDN_EN (0x01 << 20)
+#define IDLE_LOCAL_ODT_0x03 (0x03 << 12)
+#define RD_LOCAL_ODT_0x02 (0x02 << 8)
+#define PHY_READ_LATENCY (0xF & (CAS_LATENCY+1))
+
+#define mDDR_EMIF_READ_LATENCY	(DYN_PWRDN_EN | IDLE_LOCAL_ODT_0x03 | RD_LOCAL_ODT_0x02 | PHY_READ_LATENCY)
+
+#define T_RP (18)
+#define T_RCD (18)
+#define T_WR (12)
+#define T_RAS (42)
+#define T_RC (60)
+#define T_RRD (12)
+#define T_WTR (12)
+
+#define ceil_div(x, y) (((x) + (y) - 1) / (y))
+#define TIM_FIELD(param, field_lsb_pos) ((ceil_div(param, mDDR_CLK_PERIOD)-1) << field_lsb_pos)
+
+#define mDDR_EMIF_TIM1  TIM_FIELD(T_RP, 25) | \
+                        TIM_FIELD(T_RCD, 21) | \
+                        TIM_FIELD(T_WR, 17) | \
+                        TIM_FIELD(T_RAS, 12) | \
+                        TIM_FIELD(T_RC, 6) | \
+                        TIM_FIELD(T_RRD, 3) | \
+                        TIM_FIELD(T_WTR, 0)
+
+#define T_XP (25)
+#define T_ODT (0) /* ODT does nto apply to mDDR */
+#define T_XSNR (200) /* (?) Using tXSR from datasheet */
+#define T_XSRD (200) /* Same as tXSR ? */
+#define T_RTP (mDDR_CLK_PERIOD) /* (?) Let's start with 1 clock period (not on datasheet) */
+#define T_CKE (200+mDDR_CLK_PERIOD) /* (?) Datasheet has drawing but no mention of the value in AC params */
+
+#define mDDR_EMIF_TIM2			TIM_FIELD(T_XP, 28) | TIM_FIELD(T_ODT, 25) | TIM_FIELD(T_XSNR, 16) |\
+                                TIM_FIELD(T_XSRD, 6) | TIM_FIELD(T_RTP, 3) | TIM_FIELD(T_CKE, 0)
+
+#define T_PDLL_UL (0) /* (?) no DLL on mDDR */
+#define T_CSTA (0) /* (?) Not applicable to our case: we only have one CS */
+#define T_ZQCS (0) /* (?) Not applicable to mDDR */
+#define T_RFC (110)
+#define T_RAS_MAX (12) /* (?) tRASmax / tREFI = 100/7.8 = 12 */
+
+
+#define mDDR_EMIF_TIM3			TIM_FIELD(T_PDLL_UL, 28) | TIM_FIELD(T_CSTA, 24) | \
+                                TIM_FIELD(T_ZQCS, 15) | TIM_FIELD(T_RFC, 4) | T_RAS_MAX
+
+#define PASR (6) /* 1/16th Array self refresh */
+#define REFRESH_RATE (1294) /* (?) mDDR_CLK_FREQ*tREFI = 166*7.8 = 1294.8 */
+#define mDDR_EMIF_REF_CTRL	(PASR << 24) | (REFRESH_RATE)
+
+#define SDRAM_TYPE (1) /* mDDR */
+#define IBANK_POS (0) /* Try the high performance mode first, see EBANK_POS too */
+#define SDRAM_DRIVE (3) /* lowest (1/8) drive strength */
+
+#define BUS_WIDTH (0) /* 0 = 32bits */
+#define ROWSIZE (4) /* 13bits rows */
+#define IBANK (2) /* 4 banks */
+#define EBANK (0) /* Use only CS0 */ 
+#define PAGESIZE (1) /* 9 columns -> 512 word page */
+	
+#define mDDR_EMIF_SDRAM_CONFIG		(SDRAM_TYPE << 29) | (IBANK_POS << 27) | (SDRAM_DRIVE << 18) |\
+                                    (BUS_WIDTH << 14)  | (CAS_LATENCY << 10) | (ROWSIZE << 7) |\
+                                    (IBANK << 4) | (EBANK << 3) | PAGESIZE
+
+#define EBANK_POS (0)
+#define mDDR_EMIF_SDRAM_CONFIG2		(EBANK_POS << 27)
+
+#define mDDR_EMIF_SDRAM_ZQCR		0x50074BE1 /* SDRAM Output Impedance Calibration Configuration */
+
+/* ASI1230 DMM LISA MAPPING: 1 contiguous 64MB section starting at 0x80000000, no interleaving */
+#define ASI1230_DMM_LISA_MAP__0		((0x80 << 24) | (2 << 20) | (1 << 8))
+#define ASI1230_DMM_LISA_MAP__1		0x0
+#define ASI1230_DMM_LISA_MAP__2		0x0
+#define ASI1230_DMM_LISA_MAP__3		0x0
