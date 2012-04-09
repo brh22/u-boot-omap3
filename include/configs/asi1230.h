82170d3c055de2d03212c435a70acd0cfc2b99c9
Delio Brignoli <dbrignoli@audioscience.com>
2012-04-09 13:31:23 +0200

    ASI1230: Updated default environment for 1st stage bootloader (MLO)

--- include/configs/asi1230.h
+++ include/configs/asi1230.h
@@ -60,7 +60,7 @@
 # define CONFIG_MMC			1
 # define CONFIG_SPI			1
 
-# define CONFIG_BOOTARGS "console=ttyO0,115200n8 rootwait root=/dev/mmcblk0p2 rw mem=64M earlyprink vram=4M ip=off noinitrd"
+# define CONFIG_BOOTARGS "console=ttyO0,115200n8 rootwait root=/dev/mmcblk0p2 rw mem=48M earlyprink ip=off noinitrd"
 # define CONFIG_PREBOOT "mmc rescan 0; fatload mmc 0 0x81000000 uImage; bootm 0x81000000"
 
 # define CONFIG_CMD_MEMORY	/* for mtest */
