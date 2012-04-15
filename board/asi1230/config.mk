1923f3eab42f5fe6c8572034f7f5147eaf05c035
Delio Brignoli <dbrignoli@audioscience.com>
2012-04-16 01:32:55 +0200

    ASI1230: Clean up compilation output so warnings can be spotted easily

--- board/asi1230/config.mk
+++ board/asi1230/config.mk
@@ -23,3 +23,63 @@ TI_LOAD_ADDR = 0x40300000
 CROSS_COMPILE:= arm-none-linux-gnueabi-
 LDSCRIPT:=board/asi1230/u-boot.lds
 
+# Attempt at making build process less verbose
+
+V = 0
+
+USE_PRIVATE_LIBGCC := $(OBJTREE)/arch/$(ARCH)/lib
+
+RELDIR = $(subst $(TOPDIR)/,,$(CURDIR))
+
+ORIG_CC := $(CC)
+CC_0 = @echo -e "gcc\t$(RELDIR)/$<" && $(ORIG_CC)
+CC_1 := $(CC)
+CC = $(CC_$(V))
+
+CPP	= $(CC) -E
+CPP_1 := $(CC_1) -E
+
+MAKE_0 := @$(MAKE) -s
+MAKE_1 := $(MAKE)
+MAKE = $(MAKE_$(V))
+
+ORIG_OBJCOPY := $(OBJCOPY)
+OBJCOPY_0 = @echo -e "objcopy\t$(RELDIR)/$<" && $(ORIG_OBJCOPY)
+OBJCOPY_1 := $(OBJCOPY)
+OBJCOPY = $(OBJCOPY_$(V))
+
+ORIG_LD := $(LD)
+LD_0 = @echo -e "ld\t$(RELDIR)/$<" && $(ORIG_LD)
+LD_1 := $(LD)
+LD = $(LD_$(V))
+
+#AS_0 := $(XECHO) -t "AS $<..."; $(AS)
+#AS_1 := $(AS)
+#AS = $(AS_$(V))
+
+#AR_0 := $(XECHO) -t "AR $<..."; $(AR)
+#AR_1 := $(AR)
+#AR = $(AR_$(V))
+
+#NM_0 := $(XECHO) -t "NM $<..."; $(NM)
+#NM_1 := $(NM)
+#NM = $(NM_$(V))
+
+#LDR_0 := $(XECHO) -t "LDR $<..."; $(LDR)
+#LDR_1 := $(LDR)
+#LDR = $(LDR_$(V))
+
+#STRIP_0 := $(XECHO) -t "STRIP $<..."; $(STRIP)
+#STRIP_1 := $(STRIP)
+#STRIP = $(STRIP_$(V))
+
+#OBJDUMP_0 := $(XECHO) -t "OBJDUMP $<..."; $(OBJDUMP)
+#OBJDUMP_1 := $(OBJDUMP)
+#OBJDUMP = $(OBJDUMP_$(V))
+
+#RANLIB_0 := $(XECHO) -t "RANLIB $<..."; $(RANLIB)
+#RANLIB_1 := $(RANLIB)
+#RANLIB = $(RANLIB_$(V))
+
+
+
--- rules.mk
+++ rules.mk
@@ -29,7 +29,7 @@ $(obj).depend:	$(src)Makefile $(TOPDIR)/config.mk $(SRCS) $(HOSTSRCS)
 		@rm -f $@
 		@for f in $(SRCS); do \
 			g=`basename $$f | sed -e 's/\(.*\)\.\w/\1.o/'`; \
-			$(CC) -M $(CPPFLAGS) -MQ $(obj)$$g $$f >> $@ ; \
+			$(CC_1) -M $(CPPFLAGS) -MQ $(obj)$$g $$f >> $@ ; \
 		done
 		@for f in $(HOSTSRCS); do \
 			g=`basename $$f | sed -e 's/\(.*\)\.\w/\1.o/'`; \
