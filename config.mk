brh22/u-boot-omap3

commit 1923f3eab42f5fe6c8572034f7f5147eaf05c035

ASI1230: Clean up compilation output so warnings can be spotted easily

config.mk
@@ -95,7 +95,7 @@ HOSTCFLAGS	+= -pedantic
 # Option checker (courtesy linux kernel) to ensure
 # only supported compiler options are used
 #
-cc-option = $(shell if $(CC) $(CFLAGS) $(1) -S -o /dev/null -xc /dev/null \
+cc-option = $(shell if $(CC_1) $(CFLAGS) $(1) -S -o /dev/null -xc /dev/null \
 		> /dev/null 2>&1; then echo "$(1)"; else echo "$(2)"; fi ;)
 
 #
@@ -162,7 +162,7 @@ endif
 endif
 OBJCFLAGS += --gap-fill=0xff
 
-gccincdir := $(shell $(CC) -print-file-name=include)
+gccincdir := $(shell $(CC_1) -print-file-name=include)
 
 CPPFLAGS := $(DBGFLAGS) $(OPTFLAGS) $(RELFLAGS)		\
 	-D__KERNEL__
@@ -197,7 +197,7 @@ AFLAGS_DEBUG :=
 
 # turn jbsr into jsr for m68k
 ifeq ($(ARCH),m68k)
-ifeq ($(findstring 3.4,$(shell $(CC) --version)),3.4)
+ifeq ($(findstring 3.4,$(shell $(CC_1) --version)),3.4)
 AFLAGS_DEBUG := -Wa,-gstabs,-S
 endif
 endif

board/asi1230/config.mk
@@ -23,3 +23,63 @@ TI_LOAD_ADDR = 0x40300000
 CROSS_COMPILE:= arm-none-linux-gnueabi-
 LDSCRIPT:=board/asi1230/u-boot.lds
 
+# Attempt at making build process less verbose
+
+V = 0
+
+USE_PRIVATE_LIBGCC := $(OBJTREE)/arch/$(ARCH)/lib
+
+RELDIR = $(subst $(TOPDIR)/,,$(CURDIR))
+
+ORIG_CC := $(CC)
+CC_0 = @echo -e "gcc\t$(RELDIR)/$<" && $(ORIG_CC)
+CC_1 := $(CC)
+CC = $(CC_$(V))
+
+CPP	= $(CC) -E
+CPP_1 := $(CC_1) -E
+
+MAKE_0 := @$(MAKE) -s
+MAKE_1 := $(MAKE)
+MAKE = $(MAKE_$(V))
+
+ORIG_OBJCOPY := $(OBJCOPY)
+OBJCOPY_0 = @echo -e "objcopy\t$(RELDIR)/$<" && $(ORIG_OBJCOPY)
+OBJCOPY_1 := $(OBJCOPY)
+OBJCOPY = $(OBJCOPY_$(V))
+
+ORIG_LD := $(LD)
+LD_0 = @echo -e "ld\t$(RELDIR)/$<" && $(ORIG_LD)
+LD_1 := $(LD)
+LD = $(LD_$(V))
+
+#AS_0 := $(XECHO) -t "AS $<..."; $(AS)
+#AS_1 := $(AS)
+#AS = $(AS_$(V))
+
+#AR_0 := $(XECHO) -t "AR $<..."; $(AR)
+#AR_1 := $(AR)
+#AR = $(AR_$(V))
+
+#NM_0 := $(XECHO) -t "NM $<..."; $(NM)
+#NM_1 := $(NM)
+#NM = $(NM_$(V))
+
+#LDR_0 := $(XECHO) -t "LDR $<..."; $(LDR)
+#LDR_1 := $(LDR)
+#LDR = $(LDR_$(V))
+
+#STRIP_0 := $(XECHO) -t "STRIP $<..."; $(STRIP)
+#STRIP_1 := $(STRIP)
+#STRIP = $(STRIP_$(V))
+
+#OBJDUMP_0 := $(XECHO) -t "OBJDUMP $<..."; $(OBJDUMP)
+#OBJDUMP_1 := $(OBJDUMP)
+#OBJDUMP = $(OBJDUMP_$(V))
+
+#RANLIB_0 := $(XECHO) -t "RANLIB $<..."; $(RANLIB)
+#RANLIB_1 := $(RANLIB)
+#RANLIB = $(RANLIB_$(V))
+
+
+

Makefile
@@ -268,7 +268,7 @@ export PLATFORM_LIBS
 # on the fly.
 LDPPFLAGS += \
 	-include $(TOPDIR)/include/u-boot/u-boot.lds.h \
-	$(shell $(LD) --version | \
+	$(shell $(LD_1) --version | \
 	  sed -ne 's/GNU ld version \([0-9][0-9]*\)\.\([0-9][0-9]*\).*/-DLD_MAJOR=\1 -DLD_MINOR=\2/p')
 
 ifeq ($(CONFIG_NAND_U_BOOT),y)
@@ -337,20 +337,21 @@ $(obj)u-boot.sha1:	$(obj)u-boot.bin
 $(obj)u-boot.dis:	$(obj)u-boot
 		$(OBJDUMP) -d $< > $@
 
-GEN_UBOOT = \
-		UNDEF_SYM=`$(OBJDUMP) -x $(LIBBOARD) $(LIBS) | \
-		sed  -n -e 's/.*\($(SYM_PREFIX)__u_boot_cmd_.*\)/-u\1/p'|sort|uniq`;\
-		cd $(LNDIR) && $(LD) $(LDFLAGS) $$UNDEF_SYM $(__OBJS) \
+$(obj)u-boot:	depend $(SUBDIRS) $(OBJS) $(LIBBOARD) $(LIBS) $(LDSCRIPT) $(obj)u-boot.lds
+		$(eval UNDEF_SYM := $(shell $(OBJDUMP) -x $(LIBBOARD) $(LIBS) | \
+				sed  -n -e 's/.*\($(SYM_PREFIX)__u_boot_cmd_.*\)/-u\1/p'|sort|uniq))
+		@cd $(LNDIR)
+		$(LD) $(LDFLAGS) $(UNDEF_SYM) $(__OBJS) \
 			--start-group $(__LIBS) --end-group $(PLATFORM_LIBS) \
 			-Map u-boot.map -o u-boot
-$(obj)u-boot:	depend $(SUBDIRS) $(OBJS) $(LIBBOARD) $(LIBS) $(LDSCRIPT) $(obj)u-boot.lds
-		$(GEN_UBOOT)
 ifeq ($(CONFIG_KALLSYMS),y)
-		smap=`$(call SYSTEM_MAP,u-boot) | \
-			awk '$$2 ~ /[tTwW]/ {printf $$1 $$3 "\\\\000"}'` ; \
-		$(CC) $(CFLAGS) -DSYSTEM_MAP="\"$${smap}\"" \
+		$(eval smap := $(shell $(call SYSTEM_MAP,u-boot) | \
+			awk '$$2 ~ /[tTwW]/ {printf $$1 $$3 "\\\\000"}'))
+		$(CC) $(CFLAGS) -DSYSTEM_MAP="\"$(smap)\"" \
 			-c common/system_map.c -o $(obj)common/system_map.o
-		$(GEN_UBOOT) $(obj)common/system_map.o
+		$(LD) $(LDFLAGS) $(UNDEF_SYM) $(__OBJS) \
+			--start-group $(__LIBS) --end-group $(PLATFORM_LIBS) \
+			-Map u-boot.map -o u-boot $(obj)common/system_map.o
 endif
 
 $(OBJS):	depend
@@ -405,7 +406,7 @@ env:
 # parallel sub-makes creating .depend files simultaneously.
 depend dep:	$(TIMESTAMP_FILE) $(VERSION_FILE) $(obj)include/autoconf.mk
 		for dir in $(SUBDIRS) $(CPUDIR) $(dir $(LDSCRIPT)) ; do \
-			$(MAKE) -C $$dir _depend ; done
+			$(MAKE_1) -s -C $$dir _depend ; done
 
 TAG_SUBDIRS = $(SUBDIRS)
 TAG_SUBDIRS += $(dir $(__LIBS))
@@ -439,14 +440,14 @@ $(obj)include/autoconf.mk.dep: $(obj)include/config.h include/common.h
 	@$(XECHO) Generating $@ ; \
 	set -e ; \
 	: Generate the dependancies ; \
-	$(CC) -x c -DDO_DEPS_ONLY -M $(HOSTCFLAGS) $(CPPFLAGS) \
+	$(CC_1) -x c -DDO_DEPS_ONLY -M $(HOSTCFLAGS) $(CPPFLAGS) \
 		-MQ $(obj)include/autoconf.mk include/common.h > $@
 
 $(obj)include/autoconf.mk: $(obj)include/config.h
 	@$(XECHO) Generating $@ ; \
 	set -e ; \
 	: Extract the config macros ; \
-	$(CPP) $(CFLAGS) -DDO_DEPS_ONLY -dM include/common.h | \
+	$(CPP_1) $(CFLAGS) -DDO_DEPS_ONLY -dM include/common.h | \
 		sed -n -f tools/scripts/define2mk.sed > $@.tmp && \
 	mv $@.tmp $@
 

examples/api/Makefile
@@ -62,7 +62,7 @@ OBJS	+= $(addprefix $(obj),$(COBJ_FILES-y))
 OBJS	+= $(addprefix $(obj),$(notdir $(EXT_COBJ_FILES-y)))
 OBJS	+= $(addprefix $(obj),$(notdir $(EXT_SOBJ_FILES-y)))
 
-gcclibdir := $(shell dirname `$(CC) -print-libgcc-file-name`)
+gcclibdir := $(shell dirname `$(CC_1) -print-libgcc-file-name`)
 
 CPPFLAGS += -I..
 

examples/standalone/Makefile
@@ -68,7 +68,7 @@ ELF	:= $(addprefix $(obj),$(ELF))
 BIN	:= $(addprefix $(obj),$(BIN))
 SREC	:= $(addprefix $(obj),$(SREC))
 
-gcclibdir := $(shell dirname `$(CC) -print-libgcc-file-name`)
+gcclibdir := $(shell dirname `$(CC_1) -print-libgcc-file-name`)
 
 CPPFLAGS += -I..
 

rules.mk
@@ -29,7 +29,7 @@ $(obj).depend:	$(src)Makefile $(TOPDIR)/config.mk $(SRCS) $(HOSTSRCS)
 		@rm -f $@
 		@for f in $(SRCS); do \
 			g=`basename $$f | sed -e 's/\(.*\)\.\w/\1.o/'`; \
-			$(CC) -M $(CPPFLAGS) -MQ $(obj)$$g $$f >> $@ ; \
+			$(CC_1) -M $(CPPFLAGS) -MQ $(obj)$$g $$f >> $@ ; \
 		done
 		@for f in $(HOSTSRCS); do \
 			g=`basename $$f | sed -e 's/\(.*\)\.\w/\1.o/'`; \

commit 35abf0c6957783ba19fe3363a6759fd3dc81af46

ASI1230: Fix cc-option assignment broken by commit 1923f3eab42f5fe6c8572034f7f5147eaf05c035

config.mk
@@ -95,7 +95,7 @@ HOSTCFLAGS	+= -pedantic
 # Option checker (courtesy linux kernel) to ensure
 # only supported compiler options are used
 #
-cc-option = $(shell if $(CC_1) $(CFLAGS) $(1) -S -o /dev/null -xc /dev/null \
+cc-option = $(shell if $(CROSS_COMPILE)gcc $(CFLAGS) $(1) -S -o /dev/null -xc /dev/null \
 		> /dev/null 2>&1; then echo "$(1)"; else echo "$(2)"; fi ;)
 
 #